aaaf8abecc1572e302be0ecbf7a377524f73130c
eskalona17 <eskalona_17@hotmail.com>
2020-05-20 18:24:55 +0200

    showing modal

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,7 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
 
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 600,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 const Receta = ({ receta }) => {
+  //configuracion del modal de material-ui
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   //extraer los valores del context
   const { guardarIdReceta } = useContext(ModalContext);
   return (
@@ -17,10 +55,24 @@ const Receta = ({ receta }) => {
           <button
             type="button"
             className="btn btn-block btn-primary"
-            onClick={() => guardarIdReceta(receta.idDrink)}
+            onClick={() => {
+              guardarIdReceta(receta.idDrink);
+              handleOpen();
+            }}
           >
             Ver receta
           </button>
+          <Modal
+            open={open}
+            onClose={() => {
+              guardarIdReceta(null);
+              handleClose();
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h1>Desde modal</h1>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
