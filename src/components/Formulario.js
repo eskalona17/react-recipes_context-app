6042708cf058a3ac0a22f956250276c5e269b9f6
eskalona17 <eskalona_17@hotmail.com>
2020-05-20 13:04:36 +0200

    crating context provider

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CategoriasContext } from "../context/CategoriasContext";
 
 const Formulario = () => {
+  const {} = useContext(CategoriasContext);
   return (
     <form action="" className="col-12">
       <fieldset className="text-center">
         <legend>Busca bebidas por categoría o Ingrediente</legend>
       </fieldset>
       <div className="row mt-4">
-          <div className="col-md-4">
-              <input type="text" name="nombre" placeholder="Buscar por ingrediente" className="form-control"/>
-          </div>
-          <div className="col-md-4">
-              <select name="categoria" className="form-control">
-                  <option value="">-- Selecciona categoria --</option>
-              </select>
-          </div>
-          <div className="col-md-4">
-              <input type="submit" className="btn btn-block btn-primary" value="Buscar bebidas"/>
-          </div>
+        <div className="col-md-4">
+          <input
+            type="text"
+            name="nombre"
+            placeholder="Buscar por ingrediente"
+            className="form-control"
+          />
+        </div>
+        <div className="col-md-4">
+          <select name="categoria" className="form-control">
+            <option value="">-- Selecciona categoria --</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar bebidas"
+          />
+        </div>
       </div>
     </form>
   );
